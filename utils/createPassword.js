ed70dae45f59651fa1b4f971c5a0fbdd7fbedc1a
Fancy Stacks <fancystacks@protonmail.com>
2021-08-18 02:37:41 -0600

    Num, symb, alphabet 🔐

---
+++ utils/createPassword.js
@@ -0,0 +1,7 @@
+const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numbers = '0123456789';
+const symbols = '!@#$%^&*';
+
+const createPassword = (length = 8, hasNumbers = true, hasSymbols = true) => {
+
+}
