cba05b6b107eac6e9dd07ebe9554f89f7043bada
Fancy Stacks <fancystacks@protonmail.com>
2021-08-18 01:42:18 -0600

    Length Option  🔐

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const program = require('commander');
 program.version('1.0.0').description('Simple Password Generator');
 
 program.option('--length <number of characters>', 'desired password length').parse();
+
+console.log(program.opts());
