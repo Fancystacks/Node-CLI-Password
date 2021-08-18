c5dbcd3aea5085ddeefec8a90012867f34373277
Fancy Stacks <fancystacks@protonmail.com>
2021-08-18 01:43:14 -0600

    Default of 8  🔐

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const program = require('commander');
 
 program.version('1.0.0').description('Simple Password Generator');
 
-program.option('--length <number of characters>', 'desired password length').parse();
+program.option('--length <number of characters>', 'desired password length', '8').parse();
 
 console.log(program.opts());
