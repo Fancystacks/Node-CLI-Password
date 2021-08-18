0d3d0d6976c622cd93a7811d15927b93befc221d
Fancy Stacks <fancystacks@protonmail.com>
2021-08-18 02:43:36 -0600

    Added Capital Option 🔐

--- utils/createPassword.js
+++ utils/createPassword.js
@@ -3,8 +3,10 @@ const capitals = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const numbers = '0123456789';
 const symbols = '!@#$%^&*';
 
-const createPassword = (length = 8, hasNumbers = true, hasSymbols = true) => {
+const createPassword = (length = 8, hasCapitals = true, hasNumbers = true, hasSymbols = true) => {
     let chars = alphabet;
     hasNumbers ? (chars + numbers) : ''
     hasSymbols ? (chars + symbols) : ''
+    hasCapitals ? (chars + capitals) : ''
+    return chars
 }
