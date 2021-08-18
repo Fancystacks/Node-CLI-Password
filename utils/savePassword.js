cd600ecc1e82ac14de552fac3a1e1b771afc9829
Fancy Stacks <fancystacks@protonmail.com>
2021-08-18 03:35:29 -0600

    Save to txt 🔐

--- utils/savePassword.js
+++ utils/savePassword.js
@@ -5,8 +5,12 @@ const chalk = require('chalk');
 
 const savePassword = (password) => {
     fs.open(path.join(__dirname, '../', 'passwords.txt'), 'a', 666, (event, id) => {
-        fs.write(id, password + os.EOL, null, 'utf-8')
-    })
+        fs.write(id, password + os.EOL, null, 'utf-8', () => {
+            fs.close(id, () => {
+                console.log(chalk.magentaBright('Password saved to passwords.txt'));
+            });
+        });
+    });
 }
 
 module.exports = savePassword;
