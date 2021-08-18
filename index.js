6f34fca308bf5db097b9c8cf45077726ca295a2b
Fancy Stacks <fancystacks@protonmail.com>
2021-08-18 01:56:38 -0600

    Command Options  🔐

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const program = require('commander');
 
 program.version('1.0.0').description('Simple Password Generator');
 
-program.option('--length <number of characters>', 'desired password length', '8').parse();
+program
+.option('--length <number of characters>', 'desired password length', '8')
+.option('--save', 'save to passwords.txt file')
+.option('-nn, --no-numbers', 'no numbers in password')
+
+.parse();
 
 console.log(program.opts());
