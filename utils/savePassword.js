84b6b471ca506f53ff1f975fa458627f2666382a
Fancy Stacks <fancystacks@protonmail.com>
2021-08-18 03:33:32 -0600

    Import File Systems 🔐

--- utils/savePassword.js
+++ utils/savePassword.js
@@ -2,3 +2,11 @@ const fs = require('fs');
 const path = require('path');
 const os = require('os');
 const chalk = require('chalk');
+
+const savePassword = (password) => {
+    fs.open(path.join(__dirname, '../', 'passwords.txt'), 'a', 666, (event, id) => {
+        fs.write(id, password + os.EOL, null, 'utf-8')
+    })
+}
+
+module.exports = savePassword;
