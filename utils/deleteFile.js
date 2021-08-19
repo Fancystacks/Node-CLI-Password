4ca49a43d68fa79d33dd66b6c2ed4755e3bf0e39
Fancy Stacks <fancystacks@protonmail.com>
2021-08-19 00:38:26 -0600

    Added --clear Confirmation 🔐

--- utils/deleteFile.js
+++ utils/deleteFile.js
@@ -3,7 +3,7 @@ const path = require('path');
 const chalk = require('chalk');
 
 const deleteFile = () => {
-    fs.writeFile('./passwords.txt', '', function(){console.log(chalk.red('Cleared from passwords.txt'))})
+    fs.writeFile('./passwords.txt', '', function(){console.log(chalk.red('Cleared history from passwords.txt'))})
 }
 
 module.exports = deleteFile;
